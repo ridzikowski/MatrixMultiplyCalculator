7d41b8add1aa7bdfb4e3f73164e6b2dbe8fd37c3
Radosaw Idzikowski <radoslaw.idzikowski@e-science.pl>
2024-04-12 11:03:58 +0200

    multiply by thread improved

--- MatrixMultiplyCalculator/Matrix.cs
+++ MatrixMultiplyCalculator/Matrix.cs
@@ -91,6 +91,8 @@ namespace MatrixMultiplyCalculator
         }
 
         public  static volatile Matrix? matrix;
+        public static Matrix? A;
+        public static Matrix? B;
         public Matrix? multiplyThread(Matrix other, int numthreads = 4) {
 
             matrix = null;
@@ -98,11 +100,13 @@ namespace MatrixMultiplyCalculator
             if (other != null && cols == other.rows)
             {
                 matrix = new Matrix(rows, other.cols);
+                A = this;
+                B = other;
                 Thread[] threads = new Thread[numthreads];
                 for (int i = 0; i < numthreads; i++)
                 {
                     var id = i;
-                    threads[i] = new Thread(() => couting(id, this, other, numthreads));
+                    threads[i] = new Thread(() => couting(id, numthreads));
                     threads[i].Name = $"{i}";
                 }
                 foreach (Thread th in threads)
@@ -113,15 +117,15 @@ namespace MatrixMultiplyCalculator
             }
             return matrix;
         }
-        static void couting(int id, Matrix my, Matrix other, int numthreads)
+        static void couting(int id, int numthreads)
         {
-            for (int v = id; v < my.rows * other.cols; v += numthreads)
+            for (int v = id; v < A.rows * B.cols; v += numthreads)
             {
 
-                int row = v / other.cols;
-                int col = v % other.cols;
-                for (int k = 0; k < my.cols; k++)
-                    matrix.values[row, col] += my.values[row, k] * other.values[k, col];
+                int row = v / B.cols;
+                int col = v % B.cols;
+                for (int k = 0; k < A.cols; k++)
+                    matrix.values[row, col] += A.values[row, k] * B.values[k, col];
             }   
         }
     }
--- MatrixMultiplyCalculator/Program.cs
+++ MatrixMultiplyCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace MatrixMultiplyCalculator
+﻿using System.Diagnostics;
+namespace MatrixMultiplyCalculator
 {
     internal class Program
     {
@@ -9,10 +10,14 @@
                 Console.WriteLine("MENU:");
                 Console.WriteLine("0. Exit");
                 Console.WriteLine("1. Simple test");
+                Console.WriteLine("2. Perforance test for Parallel");
+                Console.WriteLine("3. Perforance test for Thread");
 
                 var opt = int.Parse(Console.ReadLine());
                 if (opt == 1) simpleTest();
                 else if (opt == 0) break;
+                else if (opt == 2) PerformanceTest("Parallel");
+                else if (opt == 3) PerformanceTest("Thread");
 
                 Console.ReadKey();
             }
@@ -36,5 +41,53 @@
             Console.WriteLine(C.check(D));
         }
 
+        static void PerformanceTest(string str)
+        {
+            //int maxTH = Math.Max(32, Environment.ProcessorCount);
+            List<int> threads = Enumerable.Range(1, Environment.ProcessorCount).ToList();
+            //List<int> sizes = Enumerable.Range(1, 4).Select(x => x *250).ToList();
+
+            Console.WriteLine("Enter the size of matrix");
+            int size = int.Parse(Console.ReadLine());
+
+            List<int> sizes = new List<int>(){ size };
+            Int64 reference = 0;
+            
+            foreach (int s in sizes)
+            {
+                Console.WriteLine($"size: {s}");
+                Int64[] times = new Int64[Environment.ProcessorCount];
+                foreach (int th in threads)
+                {
+                    Console.Write($"th: {th} ");
+                    for (int seed = 1; seed < 5; seed++)
+                    {
+                        Console.Write($"seed: {seed} ");
+                        Matrix A = new Matrix(s, s, seed);
+                        Matrix B = new Matrix(s, s, seed + 100);
+
+                        var watch = Stopwatch.StartNew();
+                        if (str == "Parallel")
+                            A.multiplyParallel(B, th);
+                        else if (str == "Thread")
+                            A.multiplyThread(B, th);
+                        watch.Stop();
+                        var elapsedMs = watch.ElapsedMilliseconds;
+                        Console.Write($"time : {elapsedMs} ");
+                        times[th - 1] += elapsedMs;
+                    }
+                }
+                times.Select(x => x / 5).ToArray();
+
+                Console.WriteLine("\n Threads");
+                Console.WriteLine(String.Join(" ",threads));
+                Console.WriteLine("times");
+                Console.WriteLine(String.Join(" ", times));
+                var speedup = times.Select(x => Math.Round((double)times[0] / (double)x,2)).ToArray();
+                Console.WriteLine("speedup");
+                Console.WriteLine(String.Join(" ", speedup));
+            }
+            
+        }
     }
 }
