d6a9ae44eea5b345e8880356d5209e078207ea5c
Radosaw Idzikowski <radoslaw.idzikowski@e-science.pl>
2024-04-12 09:43:21 +0200

    Add project files.

---
+++ MatrixMultiplyCalculator/Matrix.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplyCalculator
+{
+    internal class Matrix
+    {
+        int rows;
+        int cols;
+        double[,] values;
+        public Matrix(int _n, int _m)
+        {
+            this.rows = _n;
+            this.cols = _m;
+            values = new double[rows, cols];
+        }
+        public Matrix(int _n, int _m, int _seed)
+        {
+            this.rows = _n;
+            this.cols = _m;
+            values = new double[rows,cols];
+            Random random = new Random(_seed);
+            for (int i = 0; i < rows; i++) 
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    values[i,j] = random.NextDouble();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    str += $"{values[i, j]:0.00} ";
+                }
+                str += "\n";
+            }
+
+            return str;
+        }
+
+        public bool check(Matrix other)
+        {
+            bool compatible = true;
+            if (cols == other.cols && rows == other.rows) 
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (values[i, j] != other.values[i, j])
+                            return false;
+                    }
+                }
+            }
+            else compatible = false;
+            return compatible;
+        }
+
+        public Matrix? multiplyParallel(Matrix other, int numthreads = 4)
+        {
+            Matrix? result = null;
+
+            if (other != null && cols == other.rows)
+            {
+                result = new Matrix(rows, other.cols);
+                ParallelOptions opt = new ParallelOptions() { MaxDegreeOfParallelism = numthreads };
+                Parallel.For(0, numthreads, opt, id =>
+                {
+                    for (int v = id; v < rows * other.cols; v += numthreads)
+                    {
+
+                        int row = v / other.cols;
+                        int col = v % other.cols;
+                        for (int k = 0; k < cols; k++)
+                            result.values[row, col] += values[row, k] * other.values[k, col];
+                    }
+                    ;
+                });
+
+            }
+            return result;
+        }
+
+        public  static volatile Matrix? matrix;
+        public Matrix? multiplyThread(Matrix other, int numthreads = 4) {
+
+            matrix = null;
+
+            if (other != null && cols == other.rows)
+            {
+                matrix = new Matrix(rows, other.cols);
+                Thread[] threads = new Thread[numthreads];
+                for (int i = 0; i < numthreads; i++)
+                {
+                    var id = i;
+                    threads[i] = new Thread(() => couting(id, this, other, numthreads));
+                    threads[i].Name = $"{i}";
+                }
+                foreach (Thread th in threads)
+                    th.Start();
+                foreach (Thread th in threads)
+                    th.Join();
+
+            }
+            return matrix;
+        }
+        static void couting(int id, Matrix my, Matrix other, int numthreads)
+        {
+            for (int v = id; v < my.rows * other.cols; v += numthreads)
+            {
+
+                int row = v / other.cols;
+                int col = v % other.cols;
+                for (int k = 0; k < my.cols; k++)
+                    matrix.values[row, col] += my.values[row, k] * other.values[k, col];
+            }   
+        }
+    }
+}
---
+++ MatrixMultiplyCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MatrixMultiplyCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("MENU:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Simple test");
+
+                var opt = int.Parse(Console.ReadLine());
+                if (opt == 1) simpleTest();
+                else if (opt == 0) break;
+
+                Console.ReadKey();
+            }
+            
+            
+        }
+        static void simpleTest()
+        {
+            Matrix A = new Matrix(4, 3, 1);
+            Matrix B = new Matrix(3, 5, 2);
+            Matrix C = A.multiplyParallel(B);
+            Matrix D = A.multiplyThread(B);
+            Console.WriteLine("A:");
+            Console.WriteLine(A);
+            Console.WriteLine("B:");
+            Console.WriteLine(B);
+            Console.WriteLine("multiply by Parallel:");
+            Console.WriteLine(C);
+            Console.WriteLine("multiply by Thread:");
+            Console.WriteLine(D);
+            Console.WriteLine(C.check(D));
+        }
+
+    }
+}
